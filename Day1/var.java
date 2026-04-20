873a3748597a500d88669acebbb14e6ed33d8c98
Kritik <kritikkaushik2004@gmail.com>
2026-04-20 23:03:26 +0530

    Create var.java

---
+++ Day1/var.java
@@ -0,0 +1,11 @@
+//This is first program variables in java
+class Main {
+public static void main(String[] args) {
+int age = 20;
+float marks = 85.5f;
+char grade = 'A';
+System.out.println(age);
+System.out.println(marks);
+System.out.println(grade);
+}
+}
