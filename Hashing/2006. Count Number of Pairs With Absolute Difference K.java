25c184d5dd7ece60025692db648e6c794112ff6a
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-08-12 08:19:52 +0530

    Create 2006. Count Number of Pairs With Absolute Difference K.java

---
+++ Hashing/2006. Count Number of Pairs With Absolute Difference K.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=0;
+
+        
+        for(int i=0;i<nums.length;i++){
+            if(map.get(nums[i]-k)!=null){
+                count+=map.get(nums[i]-k);
+            }
+            if(map.get(nums[i]+k)!=null){
+                count+=map.get(nums[i]+k);
+            }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        return count;
+    }
+}
