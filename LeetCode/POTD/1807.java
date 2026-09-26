01ce09290c73a41a810a4fdb2314f3509e0bcbcc
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-09-26 20:40:57 +0530

    Create 1807.java

---
+++ LeetCode/POTD/1807.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        Map<String, String> map = new HashMap<>();
+
+        // Store key -> value
+        for (List<String> pair : knowledge) {
+            map.put(pair.get(0), pair.get(1));
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); ) {
+            if (s.charAt(i) == '(') {
+                int j = i + 1;
+
+                // Find closing bracket
+                while (s.charAt(j) != ')') {
+                    j++;
+                }
+
+                String key = s.substring(i + 1, j);
+
+                // Replace with value or '?'
+                ans.append(map.getOrDefault(key, "?"));
+
+                i = j + 1;
+            } else {
+                ans.append(s.charAt(i));
+                i++;
+            }
+        }
+
+        return ans.toString();
+    }
+}
